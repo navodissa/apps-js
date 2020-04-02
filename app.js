21a6bcebe0dbebbe320223b5ca7dc459239e8e29
Navoda Dissanayake <navomails@gmail.com>
2020-04-02 19:22:31 +0530

    Added a http server

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const log = require('./logger');
+const http = require('http');
 
-log("Hey There");
+const server = http.createServer((req, res) => {
+    if (req.url === '/hello') {
+        res.write('Hello World');
+        res.end();
+    }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000');
